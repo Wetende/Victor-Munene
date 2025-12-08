b9af4ec0dad81009801c07bca23947cc5141f1fe
wetende <cyprianwetende@gmail.com>
2025-12-09 06:17:06 +1100

    fix

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <LazyMotion features={domMax}>
     <ParallaxProvider>
-      <BrowserRouter basename="/Victor-Munene" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <BrowserRouter basename="/portfolio" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <App />
       </BrowserRouter>
     </ParallaxProvider>
